2049eb6a832965cde41a77b304beeaaf0e444962
MaksiWWGaming <73423265+MaksiWWGaming@users.noreply.github.com>
2023-04-20 13:27:51 +0200

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Sql;
+using System.Data.SqlClient;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection Kon = new SqlConnection(@"Data Source=DEGENERATION\SQLEXPRESS;Initial Catalog=EIT-A02;Integrated Security=True");
+        SqlCommand Kom = new SqlCommand();
+
+        private void Ucitaj()
+        {
+            ListaViewa.Items.Clear();
+            Kom.Parameters.Clear();
+            Kom.Connection = Kon;
+            Kom.CommandText = "SELECT * FROM Autor";
+            Kon.Open();
+            SqlDataReader Dr = Kom.ExecuteReader();
+            while (Dr.Read())
+            {
+                ListViewItem item = new ListViewItem(Dr[0].ToString());
+                item.SubItems.Add(Dr[1].ToString());
+                item.SubItems.Add(Dr[2].ToString());
+                item.SubItems.Add(((DateTime)Dr[3]).ToString("dd.MM.yyyy"));
+                ListaViewa.Items.Add(item);
+            }
+            Kon.Close();
+        }
+
+        private void Resetuj()
+        {
+            TxtBoxSifra.Clear();
+            TxtBoxIme.Clear();
+            TxtBoxPrezime.Clear();
+            TxtBoxRodjen.Clear();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Ucitaj();
+            Resetuj();
+        }
+
+        int id = -1;
+        private void ListaViewa_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            foreach (ListViewItem item in ListaViewa.SelectedItems)
+            {
+                id = Convert.ToInt32(item.SubItems[0].Text);
+                TxtBoxSifra.Text = item.SubItems[0].Text;
+                TxtBoxIme.Text = item.SubItems[1].Text;
+                TxtBoxPrezime.Text = item.SubItems[2].Text;
+                TxtBoxRodjen.Text = item.SubItems[3].Text;
+            }
+        }
+
+        private void TSBBrisanje_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (id > 0)
+                {
+                    Kom.Connection = Kon;
+                    Kom.CommandText = "DELETE FROM Autor WHERE AutorID = @a";
+                    Kom.Parameters.Clear();
+                    Kom.Parameters.AddWithValue("@a", id);
+                    Kon.Open();
+                    Kom.ExecuteNonQuery();
+                    MessageBox.Show("Uspesno");
+                    Kon.Close();
+                    Ucitaj();
+                    Resetuj();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                Kon.Close();
+            }
+        }
+
+        private void TSBAnaliza_Click(object sender, EventArgs e)
+        {
+            Form2 form = new Form2();
+            form.ShowDialog();
+        }
+
+        private void TSBOAPK_Click(object sender, EventArgs e)
+        {
+            Form3 form = new Form3();
+            form.ShowDialog();
+        }
+
+        private void TSBIzlaz_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
---
+++ Form2.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Sql;
+using System.Data.SqlClient;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection Kon = new SqlConnection(@"Data Source=DEGENERATION\SQLEXPRESS;Initial Catalog=EIT-A02;Integrated Security=True");
+        SqlCommand Kom = new SqlCommand();
+
+        private void PopuniCBX()
+        {
+            CBXAutor.Items.Clear();
+            Kom.Connection = Kon;
+            Kom.CommandText = "SELECT DISTINCT Ime, Prezime FROM Autor";
+            Kon.Open();
+            SqlDataReader Dr = Kom.ExecuteReader();
+            while (Dr.Read())
+            {
+                string s = Dr[0].ToString() + " " + Dr[1].ToString();
+                CBXAutor.Items.Add(s);
+            }
+            Kon.Close();
+        }
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            PopuniCBX();
+        }
+
+        private void BtnPrikazi_Click(object sender, EventArgs e)
+        {
+            if (CBXAutor.Text != "" && NumUDGodine.Value > 0)
+            {
+                try
+                {
+                    Kom.Connection = Kon;
+                    Kom.Parameters.Clear();
+                    int g = (int)NumUDGodine.Value;
+                    int gg = 2023 - g;
+                    string s = CBXAutor.Text;
+                    var delovi = s.Split(' ');
+                    Kom.CommandText = @"SELECT YEAR(dbo.Na_Citanju.DatumUzimanja) AS Godina, COUNT(dbo.Na_Citanju.KnjigaID) AS Broj
+                                        FROM dbo.Na_Citanju INNER JOIN 
+                                        dbo.Knjiga ON dbo.Na_Citanju.KnjigaID = dbo.Knjiga.KnjigaID INNER JOIN 
+                                        dbo.Napisali ON dbo.Knjiga.KnjigaID = dbo.Napisali.KnjigaID INNER JOIN 
+                                        dbo.Autor ON dbo.Napisali.AutorID = dbo.Autor.AutorID 
+                                        WHERE (dbo.Autor.Prezime = @p) AND (dbo.Autor.Ime =@i) AND (YEAR(dbo.Na_Citanju.DatumUzimanja) BETWEEN 2022-@gg AND 2022)
+                                        GROUP BY YEAR(dbo.Na_Citanju.DatumUzimanja)";
+                    Kon.Open();
+                    Kom.Parameters.AddWithValue("@i", delovi[0]);
+                    Kom.Parameters.AddWithValue("@p", delovi[1]);
+                    Kom.Parameters.AddWithValue("@gg", gg);
+                    SqlDataAdapter Da = new SqlDataAdapter(Kom); 
+                    DataTable Dt = new DataTable();
+                    Da.Fill(Dt);
+                    DataGridaViewa.DataSource = Dt;
+                    Kon.Close();
+                    Charta.Series.Clear();
+                    Charta.Series.Add("Prikaz");
+                    foreach (DataRow item in Dt.Rows)
+                    {
+                        Charta.Series["Prikaz"].Points.AddXY(item[0].ToString(), item[1]);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                } finally
+                {
+                    Kon.Close();
+                }
+            }
+        }
+
+        private void BtnIzadji_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
